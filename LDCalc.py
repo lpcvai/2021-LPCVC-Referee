c8ca73b17c8dbb4bea22a09a200e00a1a749b18f
avanishsubbiah <40191253+avanishsubbiah@users.noreply.github.com>
2020-02-10 01:09:54 -0500

    Updated LDCalc.py to Ignore Order
    This can be tested by running the code, dispite A.txt answers being out of order in both time in video and word order the error will be outputed as 0.

--- LDCalc.py
+++ LDCalc.py
@@ -1,5 +1,4 @@
 # install with pip3 install python-Levenshtein
-import re
 from Levenshtein import distance
 from collections import OrderedDict
 
@@ -9,42 +8,28 @@ class answerReader:
     # Method to read a text file: "txtName"
     def reader(txtName):
         with open(txtName) as txt:
-            aDict = OrderedDict()
+            dict = OrderedDict()
             # Reading all lines in A.txt into keysAndVals
             keysAndValsOG = txt.readlines()
             i = 0
             key = []  # Seperate array to store keys
             val = []  # Seperate array to store values
-            ''' outline in meeting 2/9
-            for val(txt.readline()):
-                if a(val) == ';':
-                    semiPos = val
-                else if a(val) == ':':
-                    a(semiPos) = '\n'
-            '''
+
             ii = 0
-            semiPos = 0;
-            for value in keysAndValsOG[0]:
-                print(keysAndValsOG[0][ii])
-            
-                if keysAndValsOG[0][ii] == ';':
-                    semiPos = ii
-                    print("; Found at ")
-                    print(semiPos)
-                elif keysAndValsOG[0][ii] == ':':
-                    if (semiPos != 0):
-                        print("------------------")
-                        print(keysAndValsOG[0][semiPos])
-                        print("------------------")
-                        keysAndValsOG[0][semiPos] = 'B'
-                    print(": Found at ")
-                    print(ii)
-                
-                
-                ii += 1
-            print(keysAndValsOG)
+            semiPos = 0
             keysAndVals = keysAndValsOG
 
+            # Converting one line default answer format to newlines
+            for value in keysAndValsOG[0]:
+                if keysAndValsOG[0][ii] == ';':
+                    semiPos = ii
+                elif keysAndValsOG[0][ii] == ':':
+                    if (semiPos != 0):
+                        keysAndVals[0] = keysAndValsOG[0][0:semiPos] + '|' + keysAndValsOG[0][(semiPos+1):]                
+                ii += 1
+            keysAndVals = keysAndValsOG[0].split("|")
+
+            # Creating dictionary for reader
             for value in keysAndVals:
                 keyVal1st = keysAndVals[i].split(":")
                 val1st = keyVal1st[1].split(";")
@@ -72,51 +57,119 @@ class answerReader:
             # For every value in key update dictionary with next key and value
             for value in key:
                 keyNow = str(key[i])
-                aDict.update({keyNow: val[i]})
+                dict.update({keyNow: val[i]})
                 i += 1
             # TODO Remove test print loop when done
-
+            '''
             print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-            for keys,values in aDict.items():
+            for keys,values in dict.items():
                 print(keys)
-                print(aDict[keys])
-            
-        #return aDict
+                print(dict[keys])
+            '''
+        return dict
 
 
 class levenshteinD:
-    # Method to calculate the distance between val in dict
+    # Method to calculate the distance between the values between two dictionaries
     def distanceCalc():
         # Initializing variables and calling reader
         realADict = answerReader.reader("realA.txt")
         aDict = answerReader.reader("A.txt")
         distList = []
+        totAns = 0
         avgDistDouble = None
-        # Iterating and calculating distance for each key in realADict
+
+        # Creating combinations of different answers for each key
+        frameAnsCombList = []
         for key in realADict:
-            # Flattening realADict values for this key into one list
+            frameAnsCombinations = [[real, gen] for real in realADict[key] for gen in aDict[key]]
+            #print(frameAnsCombinations)
+            frameAnsCombList.append(frameAnsCombinations)
+        keyInd = 0
+        for key in frameAnsCombList:
+            ansMacMatchInd = 0
+            for ansMacMatch in frameAnsCombList[keyInd]:
+                frameAnsCombList[keyInd][ansMacMatchInd] = [[real, gen] for real in frameAnsCombList[keyInd][ansMacMatchInd][0] for gen in frameAnsCombList[keyInd][ansMacMatchInd][1]]
+                ansMacMatchInd += 1
+            keyInd += 1
+        '''
+        print("-----------------------")
+        print(frameAnsCombList[0][0][0])
+        '''
+        # Creating list of distances between each possible combination of answers to each key
+        distList = frameAnsCombList
+        keyInd = 0
+        for key in frameAnsCombList:
+            ansMacMatchInd = 0
+            for ansMacMatch in frameAnsCombList[keyInd]:
+                ansInnMatchInd = 0
+                for ansInnMatch in frameAnsCombList[keyInd][ansMacMatchInd]:
+                    '''
+                    print("--------------------------------")
+                    print(keyInd)
+                    print(ansMacMatchInd)
+                    print(ansInnMatchInd)
+                    print(distance(distList[keyInd][ansMacMatchInd][ansInnMatchInd][0],distList[keyInd][ansMacMatchInd][ansInnMatchInd][1]))
+                    print(distList)
+                    '''
+                    distList[keyInd][ansMacMatchInd][ansInnMatchInd] = distance(distList[keyInd][ansMacMatchInd][ansInnMatchInd][0],distList[keyInd][ansMacMatchInd][ansInnMatchInd][1])
+                    ansInnMatchInd += 1
+                ansMacMatchInd += 1
+            keyInd += 1
+
+        # Flattening distList (There is unecessary nested lists right now)
+        keyInd = 0
+        for key in distList:
             flatRealADictStr = []
-            for sublist in realADict[key]:
+            for sublist in distList[keyInd]:
                 for val in sublist:
                     flatRealADictStr.append(val)
-            realADict[key] = flatRealADictStr
-            # print(" ".join(realADict[key]))
-            # Flattening aDict values for this key into one list
-            flatADictStr = []
-            for sublist in aDict[key]:
-                for val in sublist:
-                    flatADictStr.append(val)
-            aDict[key] = flatADictStr
-            # print(" ".join(aDict[key]))
-            # Appending the distance between
-            distList.append(distance(" ".join(" ".join(realADict[key])),
-                                     " ".join(" ".join(aDict[key]))))
+            distList[keyInd] = flatRealADictStr
+            keyInd += 1
+        #print("---------------------------------------------------")
+        #print(distList)
+
+        # Applying selection sort (least to greatest) to distList
+        keyInd = 0
+        minI = 0
+        for key in distList:
+            for i in range(len(distList[keyInd])):
+                # Find the minimum element in remaining
+                minI = i
+                for j in range(i+1, len(distList[keyInd])):
+                    if distList[keyInd][minI] > distList[keyInd][j]:
+                        minI = j
+                # Swap the found minimum element with minI       
+                distList[keyInd][i], distList[keyInd][minI] = distList[keyInd][minI], distList[keyInd][i] 
+        #print(distList)
+
+        # Creating a related array to distList with the number of words per key in realADict
+        totalWord = []
+        for key in realADict:
+            totalWordInKey = []
+            for val in realADict[key]:
+                #print(len(realADict[key]))
+                totalWordInKey.append(len(realADict[key]))
+            totalWord.append(sum(totalWordInKey))
+        #print(totalWord)
+
+        # Calculating total Levenshtein Distance per key in distListSumOfKey list 
+        distListSumOfKey = []
+        keyInd = 0
+        for key in distList:
+            #print(distList[keyInd][0:totalWord[keyInd]])
+            distListSumOfKey.append(sum(distList[keyInd][0:totalWord[keyInd]]))
+            keyInd += 1
+        #print(distListSumOfKey)
+        #print(frameAnsCombList[0][0])
+        #print([[real, gen] for real in frameAnsCombList[0][0][0] for gen in frameAnsCombList[0][0][1]])
+
         # Calculating and Returning average distance
-        if (len(distList) != 0):
-            avgDistDouble = sum(distList)/len(distList)
+        if (len(distListSumOfKey) != 0):
+            avgDistDouble = sum(distListSumOfKey)/len(distListSumOfKey)
+        
         return avgDistDouble
 
-
-#avgDist = levenshteinD.distanceCalc()
-answerReader.reader("realA.txt")
-print(avgDist)
+print("Import levenshteinD and answerReader classes instead and run levenshteinD.distanceCalc()")
+avgDist = levenshteinD.distanceCalc()
+print("The error of the solution is: %f" %(avgDist))
