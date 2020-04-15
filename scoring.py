2a3d43a640ff25603db850e0ab74684db5873a0a
Karthik Maiya <karthik.maiya@gmail.com>
2020-04-15 16:42:52 -0400

    Updated scoring to be just accuracy / power

--- scoring.py
+++ scoring.py
@@ -14,23 +14,23 @@ def parsePowerFile(powerFile):
         return power
     return -1
 
-def calc_final_score(groundTruthFile, submissionFile, powerFile, videoLength):
+def calc_final_score(groundTruthFile, submissionFile, powerFile):
     if powerFile == None:
         return 0
-    videoLengthHours = videoLength / 3600.0
+    #videoLengthHours = videoLength / 3600.0
     ldError = distanceCalc(groundTruthFile, submissionFile)
     ldAccuracy = 1 - ldError
     power = parsePowerFile(powerFile)
-    final_score_a = ldAccuracy / (1 + power) if power != -1 else 0
-    final_score_b = ldAccuracy * (1 - (power / (3 * MAX_POWER * videoLengthHours))) if power != -1 else 0
-    return (ldAccuracy, power, round(final_score_a,5), round(final_score_b,5))
+    final_score_a = ldAccuracy / (power) if power != -1 else 0
+    #final_score_b = ldAccuracy * (1 - (power / (3 * MAX_POWER * videoLengthHours))) if power != -1 else 0
+    return (ldAccuracy, power, round(final_score_a,5))
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 5:
-        final_tuple = calc_final_score(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]))
+    if len(sys.argv) == 4:
+        final_tuple = calc_final_score(sys.argv[1], sys.argv[2], sys.argv[3])
         print(final_tuple)
     else:
         print("Incorrect number of arguments. Found {:d}, expected 5".format(len(sys.argv)))
-        print("Usage: python3 scoring.py [ground truth file] [submission file] [power csv file] [video length in seconds]")
+        print("Usage: python3 scoring.py [ground truth file] [submission file] [power csv file]]")
