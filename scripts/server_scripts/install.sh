d408874bb94a5e519cd7c8a9d210e6ab93eb673f
zjiao19 <zjiao19@outlook.com>
2021-07-30 20:10:02 -0400

    reset upstream

--- scripts/server_scripts/install.sh
+++ scripts/server_scripts/install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")" || exit
 cd ../../
 git fetch origin
-git reset --hard origin/master
+git reset --hard remotes/origin/master
 pip uninstall cam2_referee -y
 python3.7 setup.py install --user
 exit
