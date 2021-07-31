6aff4f22d02eed6d9792a167c5ecbc8d3d882455
zjiao19 <zjiao19@outlook.com>
2021-07-31 09:51:47 -0400

    don't exit when submitted file doesn't exist

--- lpcvc/LPCVC2021/scoring/read_solution.py
+++ lpcvc/LPCVC2021/scoring/read_solution.py
@@ -7,9 +7,11 @@ def convert_frame_number_into_int(list_items):
         for i, item in enumerate(list_items):
             if 'frame' in item:
                 list_items[i]['frame'] = int(item['frame'])
+        return 0
     except ValueError:
-        print('Error: Value Error Inside Code', file=sys.stderr)
-        exit()
+        print('convert_frame_number_into_int Error: Value Error Inside Code', file=sys.stderr)
+        return -1
+        # exit()
 
 
 def get_dict_from_solution(file_name):
@@ -17,10 +19,11 @@ def get_dict_from_solution(file_name):
         fp = open(file_name)
         csv_read = csv.DictReader(fp)
         response = [{k.lower(): v for k, v in i.items() if k is not None} for i in csv_read]
-        convert_frame_number_into_int(response)
+        if convert_frame_number_into_int(response) == -1:
+            response = []
         fp.close()
     except FileNotFoundError:
         print('Error: File Does not Exists', file=sys.stderr)
-        response = None
-        exit()
+        response = []
+        # exit()
     return response
